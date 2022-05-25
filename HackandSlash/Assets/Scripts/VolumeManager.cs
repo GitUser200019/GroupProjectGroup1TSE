983d012fc13b23846b413850cb3367bfa606cbea
GitUser200019 <79577176+GitUser200019@users.noreply.github.com>
2022-05-25 18:30:30 +0100

    menu and volume manager

---
+++ HackandSlash/Assets/Scripts/VolumeManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeManager : MonoBehaviour
+{
+    public static VolumeManager Instance;
+
+    [SerializeField] private AudioSource _music;
+
+    void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Update()
+    {
+        //volume  = 0;
+        if(Input.GetKeyDown(KeyCode.O))
+        {
+            ChangeMasterVolume(0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ChangeMasterVolume(0.358f);
+        }
+    }
+
+    public void ChangeMasterVolume(float value)
+    {
+        AudioListener.volume = value;
+    }
+    
+}
