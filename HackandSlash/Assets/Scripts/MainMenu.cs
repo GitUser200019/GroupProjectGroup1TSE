85b6a2decb437c41deb4260f08dcd306e1683f36
GitUser200019 <79577176+GitUser200019@users.noreply.github.com>
2022-05-25 19:04:24 +0100

    controls

--- HackandSlash/Assets/Scripts/MainMenu.cs
+++ HackandSlash/Assets/Scripts/MainMenu.cs
@@ -5,10 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject controlPanel;
+
+    void Start()
+    {
+        controlPanel.SetActive(false);   
+    }
+
     public void StartButton()
     {
         SceneManager.LoadScene(1);
     }
+
+    public void controlButton()
+    {
+        controlPanel.SetActive(true);
+    }
+
+    public void closeCTRLpan()
+    {
+        controlPanel.SetActive(false);
+    }
+
 
     public void QuitButton()
     {
